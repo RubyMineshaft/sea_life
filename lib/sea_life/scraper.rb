98469b6d111fd523aca428485d21ed34b2b79fe7
Matt Etress <mattetress@me.com>
2018-10-14 19:12:56 +0000

    complete scrape_categories method

--- lib/sea_life/scraper.rb
+++ lib/sea_life/scraper.rb
@@ -1,7 +1,17 @@
 class SeaLife::Scraper
 
+  BASE_URL = "https://oceana.org"
+
   def self.scrape_categories
-    
+    categories = []
+    doc = Nokogiri::HTML(open(BASE_URL + "/marine-life"))
+    doc.css("article.animal-tile").each do |item|
+      category = {}
+      category[:url] = item.css("div.overlay a").attribute("href").value
+      category[:name] = item.css("div.copy h1").text
+      categories << category
+    end
+    categories
   end
 
 end
