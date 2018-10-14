25896777f5792ffad5155a9bf9cf4655440c4104
Matt Etress <mattetress@me.com>
2018-10-14 19:19:28 +0000

    updated make_categories method

--- lib/sea_life/cli.rb
+++ lib/sea_life/cli.rb
@@ -13,16 +13,20 @@ DOC
   end
 
   def list_categories
+    puts ""
     puts "Please choose a category to learn about:"
     puts ""
     @categories = SeaLife::Category.all
     @categories.each_with_index do |category, i|
       puts "#{i + 1}. #{category.name}"
     end
+    puts ""
   end
 
   def make_categories
-    SeaLife::Scraper.scrape_categories
+    SeaLife::Scraper.scrape_categories.each do |category|
+      SeaLife::Category.new(category)
+    end
   end
 
 end
