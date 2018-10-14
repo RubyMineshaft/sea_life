150caeea525fcf7b7c3d52637dee2cfad0142fdb
Matt Etress <mattetress@me.com>
2018-10-14 20:02:51 +0000

    define category=

--- lib/sea_life/animal.rb
+++ lib/sea_life/animal.rb
@@ -1,8 +1,14 @@
 class SeaLife::Animal
-  attr_accessor :name, :category, :distribution, :habitat, :habits, :status, :taxonomy, :short_desc, :longer_desc
+  attr_accessor :name, :distribution, :habitat, :habits, :status, :taxonomy, :short_desc, :longer_desc
+  attr_reader :category
 
   def initialize(info)
     info.each { |k, v| self.send("#{k}=", v) }
+  end
+
+  def category=(category_name)
+    @category = SeaLife::Category.find_by_name(category_name)
+    @category.animals << self
   end
 
 
